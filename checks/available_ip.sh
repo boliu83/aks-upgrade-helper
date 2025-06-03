d374a986016d0fe0ec94071a5a15c9dd715c02b8
boliu83 <boliu83@github.com>
2025-06-03 12:27:40 +1000

    updated available_ip.sh

--- checks/available_ip.sh
+++ checks/available_ip.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-explain="Not all agent pools have enough available IPs for surge requirements."
-
+# This script checks if each agent pool in an AKS cluster has enough available IPs in its subnet
 function run_check() {
     has_enough_ips="true"
     for ap in $AGENTPOOLS; do
         if [[ "${AGENTPOOL_SUBNET_AVAILABLE_IPS[${ap}]}" -lt ${AGENTPOOL_SURGEIP_REQUIRED[${ap}]} ]]; then
-
             break # If any agent pool does not have enough available IPs, we break the loop
         fi
     done
